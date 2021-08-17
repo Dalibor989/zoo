ca8ca40d3c9e77419b5a619c44ed29fb7f50e362
Dalibor989 <lazic_dalibor@hotmail.com>
2021-08-17 20:06:21 +0200

    zadatak 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <AnimalList />;
+}
+
+function AnimalList() {
+  const animals = [
+    {species: 'Sloth', name: 'Sid', dateOfBirth: new Date()},
+    {species: 'Penguin', name: 'Ricko', dateOfBirth: new Date()},
+    {species: 'Lion', name: 'Simba', dateOfBirth: new Date()},
+    {species: 'Elephant', name: 'Dumbo', dateOfBirth: new Date()},
+    {species: 'Giraffe', name: 'Melvin', dateOfBirth: new Date()},
+  ];
+
+  return (    
+    <div>
+      <h1>Animals</h1>
+      <table>
+        <tr>
+          <th>
+            Species
+          </th>
+          <th>
+            Name
+          </th>
+          <th>
+            Date of birth
+          </th>
+        </tr>
+        {animals.map((animal, index) => (
+          <tr>
+            <td key={index}>{animal.species}</td>
+            <td key={index}>{animal.name}</td>
+            <td key={index}>{animal.dateOfBirth.toLocaleDateString()}</td>
+          </tr>
+        ))}
+        
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
